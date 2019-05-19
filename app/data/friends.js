1cf47f3169c93589db87343b30e6f55a9d053b98
Jason Moore <jason.moore1369@gmail.com>
2019-05-19 12:50:11 -0500

    apiRoutes.js GET and POST added. object friend now includes routeName

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,16 +1,19 @@
-//init commit//
+
 let friends = [
     {
+        routeName: "lurtz",
         name: "Lurtz",
         photo: "https://vignette.wikia.nocookie.net/lotr/images/b/bf/Lurtz_2.jpg/revision/latest?cb=20110527083830",
         scores: [5,5,1,1,4,1,5,1,5,1]
     },
     {
+        routeName: "mr.peanut",
         name: "Mr. Peanut",
         photo: "https://d3bguyhblutwd5.cloudfront.net/1170/studio/assets/v1519372691969_6067827/manispec.png",
         scores: [1,3,4,4,2,3,3,4,2,5]
     },
     {
+        routeName: "wednesdayaddams",
         name: "Wednesday Addams",
         photo: "https://vignette.wikia.nocookie.net/addamsfamily/images/e/ee/1991.jpg/revision/latest/scale-to-width-down/310?cb=20170323180721",
         scores: [4,3,4,1,3,2,4,2,4,2]
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,1 +1,18 @@
-//init commit//
+//Displays all friends//
+app.get("/api/friends", function(req, res) {
+    return res.json(friends);
+});
+
+//POST for incoming survey data and compatability logic//
+app.post("/api/friends", function(req, res) {
+    let newFriend = req.body;
+  
+    // Using a RegEx Pattern to remove spaces from newCharacter https://www.regexbuddy.com/regex.html//
+    newFriend.routeName = newFriend.name.replace(/\s+/g, "").toLowerCase();
+  
+    console.log(newFriend);
+  
+    friends.push(newFriend);
+  
+    res.json(newFriend);
+});
