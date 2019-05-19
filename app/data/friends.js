18f992e348accdb5cd8e061ab01080159c6187d0
Jason Moore <jason.moore1369@gmail.com>
2019-05-19 11:15:07 -0500

    added 3 test friends to friends.js and provided extra photo folder as reference photos

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,1 +1,18 @@
 //init commit//
+let friends = [
+    {
+        name: "Lurtz",
+        photo: "https://vignette.wikia.nocookie.net/lotr/images/b/bf/Lurtz_2.jpg/revision/latest?cb=20110527083830",
+        scores: []
+    },
+    {
+        name: "Mr. Peanut",
+        photo: "https://d3bguyhblutwd5.cloudfront.net/1170/studio/assets/v1519372691969_6067827/manispec.png",
+        scores: []
+    },
+    {
+        name: "Wednesday Addams",
+        photo: "https://vignette.wikia.nocookie.net/addamsfamily/images/e/ee/1991.jpg/revision/latest/scale-to-width-down/310?cb=20170323180721",
+        scores: []
+    }
+];
