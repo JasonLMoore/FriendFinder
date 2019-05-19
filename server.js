725cfae52ef4f3303c162e1d85b56f9283a6339e
Jason Moore <jason.moore1369@gmail.com>
2019-05-19 12:53:02 -0500

    server.js app.listen added

--- server.js
+++ server.js
@@ -3,6 +3,11 @@ const path = require("path");
 
 const app = express();
 const PORT = 8080;
+//var PORT = process.env.PORT || 3000;//
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+});
