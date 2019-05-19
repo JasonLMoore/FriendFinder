9a00facf8ff2536c755d1902e920bbcbee1d9e2c
Jason Moore <jason.moore1369@gmail.com>
2019-05-19 17:14:14 -0500

    friendsData.push is not a function

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,9 +1,9 @@
-const path = require("path");
+friendsData = require('../data/friends.js');
 
 module.exports = function(app) {
     //Displays all friends//
     app.get("/api/friends", function (req, res) {
-        return res.json(friends);
+        res.json(friends);
     });
 
 
@@ -16,8 +16,8 @@ module.exports = function(app) {
 
         console.log(newFriend);
 
-        friends.push(newFriend);
+        friendsData.push(newFriend);
 
-        res.json(newFriend);
+        res.json(friends);
     });
 }
--- server.js
+++ server.js
@@ -8,8 +8,10 @@ let PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-require('./app/routing/htmlRoutes')(app);
+
+
 require('./app/routing/apiRoutes')(app);
+require('./app/routing/htmlRoutes')(app);
 
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
